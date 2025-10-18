b112b00b65f703cca2fcf4e721a71e26f3d9e2c3
ModEraNoah <klauck02@outlook.com>
2025-10-18 17:23:15 +0200

    feat: generic Popup component

---
+++ src/Popup.tsx
@@ -0,0 +1,40 @@
+import {
+  type RefObject,
+  type Dispatch,
+  type SetStateAction,
+  useEffect,
+} from "react";
+
+interface PopupRef {
+  ref: RefObject<null>;
+  setPopup: Dispatch<SetStateAction<boolean>>;
+  element?: any;
+}
+
+export function Popup({ ref, setPopup, element }: PopupRef) {
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      //@ts-expect-error due to never type of current
+      if (ref.current && !ref.current.contains(event.target)) {
+        setPopup(false); // Close popup if clicked outside
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={ref}
+      className="absolute text-center right-0 left-0 m-auto z-100 top-1/7 bg-gray-400 w-120 h-80 px-10 rounded-xl"
+    >
+      {element}
+    </div>
+  );
+}
